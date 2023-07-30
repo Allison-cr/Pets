591a3679d7e30bf5cd7788ea18126f42a509d995
Allison <suprun-s@bk.ru>
2023-07-30 16:50:49 +0300

    Initial Commit

---
+++ pets/petsApp.swift
@@ -0,0 +1,17 @@
+//
+//  petsApp.swift
+//  pets
+//
+//  Created by Alexander Suprun on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct petsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
